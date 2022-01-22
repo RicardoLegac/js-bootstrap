df465ffaf9cd50946cfc0e7fd06343e1bb5c7510
ricklegac <rick.legac@gmail.com>
2022-01-22 20:40:08 -0300

    push automatico, tiempo: 2022-01-22 20:40:08 -03

--- javascript/sal.js
+++ javascript/sal.js
@@ -1,2 +1,7 @@
 sal="Hola Mundo desde Javascript";
 console.log(sal);
+
+//operacion ternaria 
+
+let resultado = (3>2) ? "verdadero" : "falso"; 
+console.log(resultado)
---
+++ javascript/verificar_numero.js
@@ -0,0 +1,4 @@
+let minumero = "45x";
+
+resultado= (isNaN(Number(minumero))) ? "No es un numero": "Es un numero";
+console.log(resultado);
