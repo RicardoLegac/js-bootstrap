9ee89f68e10e11043136e48521efa6a12faa6d48
ricklegac <rick.legac@gmail.com>
2022-01-23 12:29:04 -0300

    push automatico, tiempo: 2022-01-23 12:29:04 -03

--- javascript/push.js
+++ javascript/push.js
@@ -5,3 +5,7 @@ for(let i=0;i<autos.length;i++){
 autos.push('Wolswagen');
 console.log(autos);
 console.log(Array.isArray(autos) ? "es un array": "no es un array");
+
+/*funcion de tipo expresion*/
+let sumar = function(a,b){ return (a+b)}; 
+console.log(sumar(2,3))
