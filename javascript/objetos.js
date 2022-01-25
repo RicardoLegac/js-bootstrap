b565757b2cd1ae214eaa40ce1aa58d4da85a68fe
ricklegac <rick.legac@gmail.com>
2022-01-24 23:35:54 -0300

    push automatico, tiempo: 2022-01-24 23:35:54 -03

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -10,3 +10,14 @@ console.log(persona.nombreCompleto());
 let persona2 = new Object(); //CREAMOS NUEVO OBJETO EN MEMORIA
 
 persona2.nombre = 'Gianna';
+
+//ACCEDER A PROPIEDADES COMO UN ARRAY
+
+console.log(persona['apellido']);
+
+//FOR IN 
+
+for(propiedad in persona){
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+}
