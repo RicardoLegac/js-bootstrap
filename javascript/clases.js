d67fb024f84bf04b01a21bd4c36a2bad4f90a7bb
ricklegac <rick.legac@gmail.com>
2022-01-26 13:23:36 -0300

    push automatico, tiempo: 2022-01-26 13:23:36 -03

--- javascript/clases.js
+++ javascript/clases.js
@@ -9,10 +9,26 @@ class Persona{
     set nombre(nombre){ //para modificar el atributo nombre
         this._nombre = nombre;
     }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
 }
-
+class Empleado extends Persona{
+    constructor(departamento){
+        this._departamento = departamento;
+    }
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+}
 let persona1 = new Persona('Ricardo','Leguizamon'); //si no se define un constructor, JavaScript agrega un vacio automatico
-console.log(persona1);
+console.log(persona1); 
 
 persona1.nombre= 'Gianna'; //con el metodo set!
 console.log(persona1.nombre);
