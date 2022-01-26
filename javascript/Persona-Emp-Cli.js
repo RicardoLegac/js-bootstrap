e665cfabd448a8d62878d3bf7d3b88929d946b85
ricklegac <rick.legac@gmail.com>
2022-01-26 20:26:20 -0300

    push automatico, tiempo: 2022-01-26 20:26:20 -03

--- javascript/Persona-Emp-Cli.js
+++ javascript/Persona-Emp-Cli.js
@@ -27,3 +27,63 @@ class Persona{
         return this._idPersona + ' ' + this._nombre + ' ' + this._apellido;
     }
 }
+
+class Empleado extends Persona{
+    static contadorEmpleados = 0 ;
+    constructor(nombre,apellido,edad,sueldo){
+        super(nombre,apellido,edad);
+        this._sueldo = sueldo;
+        this._idEmpleado = Empleado.contadorEmpleados++;
+    }
+    get getIdEmpleado(){
+        return this._idEmpleado;
+    }
+    get getSueldo(){
+        return this._sueldo;
+    }
+    set setSueldo(sueldo){
+        this._sueldo = sueldo;
+    }
+    toString(){
+        return super.toString() + ' ' + this._idEmpleado;
+    }
+}
+
+class Cliente extends Persona{
+    static contadorClientes = 0 ;
+    
+    constructor(nombre,apellido,edad){
+        super(nombre,apellido,edad);
+        this._idCliente = Cliente.contadorClientes++;
+        this._fecha = fecha();
+    }
+    get getIdCliente(){
+        return this._idCliente;
+    }
+    get getFechaRegistro(){
+        return this._fecha;
+    }
+    set setFechaRegistro(fecha){
+        this._fecha = fecha;
+    }
+    toString(){
+        super.toString() + ' ' + this._fecha; 
+    }
+}
+function fecha(){
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = dd + '/' + mm+ '/'+yyyy;
+    return today;
+}
+
+
+let persona = new Persona('Ricardo','Leguizamon',24);
+console.log(persona);
+let persona1 = new Persona('Gianna','Ozuna',2);
+console.log(persona1);
+
+let empleado = new Empleado('Rick','Legac',24,25000);
+console.log(empleado);
