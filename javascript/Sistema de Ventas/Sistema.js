7cf0a040e639582de7a33e83dff5685a9647ca9e
ricklegac <rick.legac@gmail.com>
2022-01-26 21:42:14 -0300

    push automatico, tiempo: 2022-01-26 21:42:14 -03

--- javascript/Sistema de Ventas/Sistema.js
+++ javascript/Sistema de Ventas/Sistema.js
@@ -5,4 +5,13 @@ class Producto{
         this._precio = precio;
         this._idProducto = Producto.contadorProductos++;
     }
+    get getIdProducto(){
+        return this._idProducto;
+    }
+    get getNombre(){
+        return this._nombre;
+    }
+    get getPrecio(){
+        return this._precio;
+    }
 }
