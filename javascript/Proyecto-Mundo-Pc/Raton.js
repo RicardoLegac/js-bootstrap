2a2905be37f500f51de6c67db308dc7755d57584
ricklegac <rick.legac@gmail.com>
2022-01-31 21:51:46 -0300

    push automatico, tiempo: 2022-01-31 21:51:46 -03

---
+++ javascript/Proyecto-Mundo-Pc/Raton.js
@@ -0,0 +1,21 @@
+class DispositivoEntrada{
+
+    constructor(tipoentrada,marca){
+        this._marca= marca;
+        this._tipoentrada = tipoentrada;
+    }
+    get getMarca(){
+        return this._marca;
+    }
+    set setMarca(marca){
+        this._marca = marca;
+    }
+    get getTipoentrada(){
+        return this._tipoentrada;
+    }
+    set setTipoentrada(tipoentrada){
+        this._tipoentrada = tipoentrada;
+    }
+
+
+}
