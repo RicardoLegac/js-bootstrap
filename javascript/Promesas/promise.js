d7593faa081ca023862ce6550d12648688e17e9a
ricklegac <rick.legac@gmail.com>
2022-02-04 15:26:09 -0300

    push automatico, tiempo: 2022-02-04 15:26:09 -03

---
+++ javascript/Promesas/promise.js
@@ -0,0 +1,15 @@
+"use strict";
+let miPromesa = new Promise((resolver,rechazar) => {
+    let expresion = true;
+    if(expresion){
+        resolver('Resuelto');
+    }else{
+        rechazar('Error');
+    }
+
+});
+miPromesa.then(
+    valor => console.log(valor),
+    error => console.log(error)
+
+); //los argumentos son funciones de tipo flecha 
