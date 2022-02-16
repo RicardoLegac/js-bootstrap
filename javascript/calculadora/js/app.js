30e4bfe1dd7ed6dc9cad44d18cdbb6b71ca0b686
ricklegac <rick.legac@gmail.com>
2022-02-16 12:42:27 -0300

    push automatico, tiempo: 2022-02-16 12:42:26 -03

--- javascript/calculadora/js/app.js
+++ javascript/calculadora/js/app.js
@@ -1,8 +1,9 @@
 console.log('xd');
 function sumar(){
+    console.log('entro en sumar');
     const forma = document.getElementById('forma');
-    let A = forma('operandoA');
-    let B = forma('operandoB');
+    let A = forma['operandoA'];
+    let B = forma['operandoB'];
     let resultado = A.value + B.value;
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
 
